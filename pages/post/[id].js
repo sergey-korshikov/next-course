7f325fef494941ad9b8321d2eb1fce42e3b825e4
korshikov <korshikov@stanki.ru>
2021-12-21 18:06:14 +0300

    fix

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,8 +1,32 @@
-import { useRouter } from "next/router"
+import { useState, useEffect } from 'react';
 import { MainLayout } from './../../components/MainLayout';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-export default function Post({ post }) {
+export default function Post({ post: serverPost }) {
+	const [post, setPost] = useState(serverPost)
+	const router = useRouter()
+
+	useEffect(() => {
+		async function load() {
+			const response = await fetch(`http://localhost:4200/posts/${router.query.id}`)
+			const data = await response.json()
+			setPost(data)
+		}
+
+		if (!serverPost) {
+			load()
+		}
+	}, [])
+
+	if (!post) {
+		return (
+			<MainLayout>
+				<p>Loading...</p>
+			</MainLayout>
+		)
+	}
+
 	return (
 		<MainLayout title={post.title}>
 			<h1>{post.title}</h1>
@@ -13,8 +37,14 @@ export default function Post({ post }) {
 	)
 }
 
-Post.getInitialProps = async (ctx) => {
-	const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`)
+Post.getInitialProps = async ({ query, req }) => {
+	if (!req) {
+		return {
+			post: null
+		}
+	}
+
+	const response = await fetch(`http://localhost:4200/posts/${query.id}`)
 	const post = await response.json()
 
 	return {
